10675313ab0eb0973573bafa69423c54349d9ef7
Ruma <matteoh8@hotmail.it>
2025-04-10 09:38:44 +0200

    init

---
+++ src/main/java/it/smartworki/dating_app/DatingAppApplication.java
@@ -0,0 +1,13 @@
+package it.smartworki.dating_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingAppApplication.class, args);
+    }
+
+}
