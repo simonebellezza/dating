e972dcc826e8b4138f490224fdbb2ade8ac36c63
Andrea Lino Posenato <andrea.posenato@yahoo.it>
2025-04-11 16:27:49 +0200

    Cartella scripts/ con file.sql

--- src/main/java/it/smartworki/dating_app/entities/User.java
+++ src/main/java/it/smartworki/dating_app/entities/User.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 @Entity
--- src/main/java/it/smartworki/dating_app/services/PreferenceService.java
+++
@@ -1,7 +0,0 @@
-package it.smartworki.dating_app.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PreferenceService {
-}
