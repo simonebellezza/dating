b243fe8376ff5909e33ad8303ec2cca6269a0f0b
Simone Bellezza <bellezzasimone80@gmail.com>
2025-04-14 12:20:31 +0200

    OK

--- src/main/java/it/smartworki/dating_app/DatingAppApplication.java
+++ src/main/java/it/smartworki/dating_app/DatingAppApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DatingAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(DatingAppApplication.class, args);
+
     }
 }
